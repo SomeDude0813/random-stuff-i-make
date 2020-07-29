89267751ca086bbaf59993f80627f13cbea83367
SomeDude0813 <68937143+SomeDude0813@users.noreply.github.com>
2020-07-29 00:13:38 -0400

    Add files via upload
    This file is to just fix the "Connected to Wi-Fi" but no internet thing. (If your Wi-Fi was simply turned off for some reason then it will obviously not work.)

---
+++ no internet fix.cmd
@@ -0,0 +1,13 @@
+netsh int ip reset
+cls
+netsh winsock reset all
+cls
+netsh int tcp reset all
+cls
+netsh int ipv4 reset all
+cls
+netsh int ipv6 reset all
+cls
+netsh int httpstunnel reset all
+cls
+ipconfig /flushdns
